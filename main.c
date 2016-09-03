bccde70ab35c3fb58d16918c273c2e4966f0883d
Melvin Lim <lim.dongxing@gmail.com>
2016-09-03 16:58:37 -0700

    try to add stand alone capability

--- main.c
+++ main.c
@@ -3,12 +3,17 @@
 #include<fcntl.h>
 #include<string.h>
 
+#include<unistd.h>
+#include<termios.h>
+
 #include<sys/resource.h>
 
 #include<sight.h>
 #include<decode.h>
 #include<graph.h>
 #include<move.h>
+
+char *cmd[] = {"./rogue", NULL};
 
 int main(int argc,char *argv[]){
 	int fdin,fdout,i,j,n;
@@ -19,11 +24,31 @@ int main(int argc,char *argv[]){
 	OBJECTS *objs;
 
 	srand(time(0));
+/*
 	if(argc<=2){
 		printf("usage: %s [output file] [process number]\n",argv[0]);
 		return 0;
 	}
-
+*/
+	int tcattr;
+	struct termios tos;
+	tcgetattr(1,&tos);
+    
+	int writepipe[2],readpipe[2];
+	pid_t pid;
+	pipe(writepipe);
+	pipe(readpipe);
+	pid=fork();
+	if (pid == 0){
+		dup2(writepipe[0],0);		//writepipe is what parent writes to.  duplicating read end of writepipe to child stdin.
+		dup2(readpipe[1],1);		//write end of readpipe to child stdout.
+		close(writepipe[0]);
+		close(readpipe[1]);
+		execvp(cmd[0],cmd);
+		printf("error, execvp failed\n");
+		return(0);
+	}
+//	printf("pipes: %d %d %d %d\n",writepipe[0],writepipe[1],readpipe[0],readpipe[1]);
 	struct rlimit rlim;
 	getrlimit(RLIMIT_STACK,&rlim);
 	rlim.rlim_cur=rlim.rlim_max;
@@ -37,24 +62,34 @@ int main(int argc,char *argv[]){
 	//rlim.rlim_cur=rlim.rlim_max=RLIM_INFINITY;
 	//setrlimit(RLIMIT_STACK,&rlim);
 	//setrlimit(RLIMIT_AS,&rlim);
-
+/*
 	sprintf(buf,"%s",argv[1]);
 	printf("target file: %s\n",buf);
-	fdout=open(buf,O_RDONLY);
+	//fdout=open(buf,O_RDONLY);
+*/
+fdout=readpipe[0];
 	if(fdout<0){
 		printf("unable to open %s\n",buf);
 		return 0;
 	}
 	printf("fdout=%d\n",fdout);
+/*
 	sprintf(buf,"/proc/%s/fd/0",argv[2]);
 	printf("target process: %s\n",buf);
-	fdin=open(buf,O_WRONLY|O_SYNC);
+//	fdin=open(buf,O_WRONLY|O_SYNC);
+*/
+fdin=writepipe[1];
 	if(fdin<0){
 		printf("unable to open %s\n",buf);
 		return 0;
 	}
 	printf("fdin=%d\n",fdin);
 //	printf("%d %d\n",fdin,fdout);
+
+	fcntl(fdin,F_SETFL,fcntl(fdin,F_GETFL)|O_NONBLOCK);
+	fcntl(fdout,F_SETFL,fcntl(fdin,F_GETFL)|O_NONBLOCK);
+	
+	tcsetattr(1,TCSANOW,&tos);
 
 	objs=createObjects();
 	objs->fdin=fdin;
@@ -79,6 +114,7 @@ int main(int argc,char *argv[]){
 
 //	objs->state=movingToStairs;
 	while(1){
+sync();
 
 		objs=scanArea(objs);
 
@@ -94,6 +130,7 @@ int main(int argc,char *argv[]){
 //		sleep(1);
 		getchar();
 		clearScreen();
+sync();
 	}
 
 	quit(fdin);
